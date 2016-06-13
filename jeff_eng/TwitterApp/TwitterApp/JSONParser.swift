9779b7294d73062a2d6f9ba0cec3dec37f0e5577
Jeff Eng <jeff@jeffeng.me>
2016-06-13 15:09:44 -0700

    defined the JSONParser class

--- jeff_eng/TwitterApp/TwitterApp/JSONParser.swift
+++ jeff_eng/TwitterApp/TwitterApp/JSONParser.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+typealias JSONParserCompletion = (success: Bool, tweets: [Tweet]?) -> ()
+
+class JSONParser {
+    class func tweetJSONFrom(data: NSData, completion: JSONParserCompletion) {
+        do {
+            if let rootObject = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as? [[String : AnyObject]] {
+                
+                var tweets = [Tweet]()
+                
+                for tweetJSON in rootObject {
+                    if let tweet = Tweet(json: tweetJSON) {
+                        tweets.append(tweet)
+                    }
+                }
+                
+                //Completion on success.
+                completion(success: true, tweets: tweets)
+            }
+        }
+        
+        catch { completion(success: false, tweets: nil) }
+    }
+    
+    class func JSONData() -> NSData {
+        guard let tweetJSONPath = NSBundle.mainBundle().URLForResource("tweet", withExtension: "json") else { fatalError("JSON file does not exist") }
+        guard let tweetJSONData = NSData(contentsOfURL: tweetJSONPath) else { fatalError("Failed to convert JSON to data.") }
+        return tweetJSONData
+    }
+}
