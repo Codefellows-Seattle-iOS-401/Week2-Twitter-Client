efbf5ff5eced5c49a626fc7d4f23dd0f434daf4d
Jeff Eng <jeff@jeffeng.me>
2016-06-13 14:31:10 -0700

    defined the User class to model the user data from the json file

--- jeff_eng/TwitterApp/TwitterApp/User.swift
+++ jeff_eng/TwitterApp/TwitterApp/User.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class User {
+    let name: String
+    let profileImageUrl: String
+    let location: String
+    
+    init?(json: [String : AnyObject]) {
+        if let name = json["name"] as? String, profileImageUrl = json["profile_image_url"] as? String, location = json["location"] as? String {
+            self.name = name
+            self.profileImageUrl = profileImageUrl
+            self.location = location
+        }
+        
+        else {
+            return nil
+        }
+    }
+}
