906c5b0c481f0c92ce2a3a873b03371f37c2072f
Jake Dobson <jake.dobson@gmail.com>
2016-10-17 14:41:01 -0700

    tweets array is displayed into tableView

--- Project/TwitterClient/TwitterClient/ViewController.swift
+++ Project/TwitterClient/TwitterClient/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    var allTweets = [Tweet]() {
+        didSet { // didSet is a property observer, will fire off once complete
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        JSONParser.tweetsFrom(data: JSONParser.sampleJSONData) { (success, results) in
+            if success {
+                if let tweets = results {
+                    self.allTweets = tweets
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
+}
+
+
+// Best practice is to write your dataSource as an extension
+
+extension ViewController: UITableViewDataSource {
+    //implement required methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTweets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath)
+        let currentTweet = self.allTweets[indexPath.row]
+        cell.textLabel?.text = currentTweet.text
+        return cell
+    }
 }
 
